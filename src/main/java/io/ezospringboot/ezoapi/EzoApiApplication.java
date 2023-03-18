51c45600553d264c770df1c09241ba9f2d01b5c3
EnverJ <Enlem1987@gmail.com>
2023-03-17 23:16:20 -0600

    ezp sping Boot

---
+++ src/main/java/io/ezospringboot/ezoapi/EzoApiApplication.java
@@ -0,0 +1,13 @@
+package io.ezospringboot.ezoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/ezospringboot/ezoapi/EzoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ezospringboot.ezoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
